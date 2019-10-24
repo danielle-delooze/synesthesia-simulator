8dfb7768f196583ffd2b1d1bc50547e055cf5020
danielle_delooze <danielle.delooze@1010data.com>
2019-10-23 22:51:44 -0400

    UI for picking colors for characters

--- simulator/src/App.vue
+++ simulator/src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <span class="app__title">Synesthesia Simulator</span>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <div v-for="(color, character) in characters" :key="character" class="app__characters">
+          <span>{{character}}</span>
+          <el-color-picker size="mini" class="app__colorpicker" v-model="characters[character]"></el-color-picker>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      characters: {A: '', B: '', C:'', D: '', E: '', F: '', G: '', H: '', I: '', J: '', K: '', L: '',
+      M: '', N: '', O: '', P: '', Q: '', R: '', S: '', T: '', U: '', V: '', W: '', X: '', Y: '', Z: ''}
+    }
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +34,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.app {
+  &__title {
+    font-weight: bold;
+    font-size: 34px;
+  }
+
+  &__characters {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  &__colorpicker {
+    margin-left: 20px;
+    vertical-align: bottom;
+  }
 }
 </style>
